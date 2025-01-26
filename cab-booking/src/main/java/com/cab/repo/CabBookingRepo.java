5f9c718f1a9b126eacba77dba739f9b0fa1a490f
Sean Parrott <74899874+Sparros@users.noreply.github.com>
2025-01-26 19:31:58 +0000

    Update CabBookingRepo.java

--- cab-booking/src/main/java/com/cab/repo/CabBookingRepo.java
+++ cab-booking/src/main/java/com/cab/repo/CabBookingRepo.java
@@ -4,5 +4,5 @@ import com.cab.entity.CabBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CabBookingRepo extends JpaRepository<CabBooking, Long> {
-    // You can define custom queries here if necessary (e.g., to find bookings by source/destination)
+    
 }
